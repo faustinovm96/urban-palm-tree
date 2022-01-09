a0bc40d2402db9957837710ac0a54dc2c2e9eeff
faustinovm96 <fvillasboa96@gmail.com>
2022-01-09 19:13:19 -0300

    Arreglo de fotos

--- 29.1 9_spring-boot-data-jpa[gran_total].zip_expanded/spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
+++ 29.1 9_spring-boot-data-jpa[gran_total].zip_expanded/spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
@@ -1,18 +1,12 @@
-package com.spingboot.app.controllers;
+package com.bolsadeideas.springboot.app.controllers;
 
-import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
-
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Map;
+
 import javax.validation.Valid;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.core.io.Resource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -24,33 +18,29 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import com.spingboot.app.models.domain.Cliente;
-import com.spingboot.app.models.service.IClienteService;
-import com.spingboot.app.models.service.IUploadFileService;
-import com.spingboot.app.util.paginator.PageRender;
+import com.bolsadeideas.springboot.app.models.entity.Cliente;
+import com.bolsadeideas.springboot.app.models.service.IClienteService;
+import com.bolsadeideas.springboot.app.models.service.IUploadFileService;
+import com.bolsadeideas.springboot.app.util.paginator.PageRender;
 
 @Controller
-@RequestMapping("/clientes")
 @SessionAttributes("cliente")
 public class ClienteController {
-	
+
 	@Autowired
-	@Qualifier("clienteServiceJPA")
 	private IClienteService clienteService;
-	
-	private final Logger log = LoggerFactory.getLogger(getClass());
-	
+
 	@Autowired
 	private IUploadFileService uploadFileService;
-	
+
 	@GetMapping(value = "/uploads/{filename:.+}")
 	public ResponseEntity<Resource> verFoto(@PathVariable String filename) {
 
@@ -67,7 +57,7 @@ public class ClienteController {
 				.header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + recurso.getFilename() + "\"")
 				.body(recurso);
 	}
-	
+
 	@GetMapping(value = "/ver/{id}")
 	public String ver(@PathVariable(value = "id") Long id, Map<String, Object> model, RedirectAttributes flash) {
 
@@ -78,36 +68,57 @@ public class ClienteController {
 		}
 
 		model.put("cliente", cliente);
-		model.put("titulo", "Detalle cliente: " + cliente.getNombres());
+		model.put("titulo", "Detalle cliente: " + cliente.getNombre());
 		return "ver";
 	}
-	
-	@GetMapping("/listar")
-	public String listar(@RequestParam(name ="page", defaultValue = "0") int page, Model model) {
-		
+
+	@RequestMapping(value = "/listar", method = RequestMethod.GET)
+	public String listar(@RequestParam(name = "page", defaultValue = "0") int page, Model model) {
+
 		Pageable pageRequest = PageRequest.of(page, 4);
-		
-		Page<Cliente> clientes = clienteService.listarClientes(pageRequest);
-		
-		PageRender<Cliente> pageRender = new PageRender<Cliente>("/clientes/listar", clientes);
-		model.addAttribute("titulo", "Listar de CLientes");
+
+		Page<Cliente> clientes = clienteService.findAll(pageRequest);
+
+		PageRender<Cliente> pageRender = new PageRender<Cliente>("/listar", clientes);
+		model.addAttribute("titulo", "Listado de clientes");
 		model.addAttribute("clientes", clientes);
 		model.addAttribute("page", pageRender);
 		return "listar";
 	}
-	
-	@GetMapping("/form")
-	public String formularioCliente(Map<String, Object> model) {
+
+	@RequestMapping(value = "/form")
+	public String crear(Map<String, Object> model) {
+
 		Cliente cliente = new Cliente();
-		
-		model.put("titulo", "Formulario de Clientes");
 		model.put("cliente", cliente);
+		model.put("titulo", "Formulario de Cliente");
 		return "form";
 	}
-	
-	@PostMapping("/form")
-	public String crearCliente(@Valid Cliente cliente, BindingResult result, @RequestParam("file") MultipartFile foto, Model model, RedirectAttributes flash, SessionStatus status) {
-		
+
+	@RequestMapping(value = "/form/{id}")
+	public String editar(@PathVariable(value = "id") Long id, Map<String, Object> model, RedirectAttributes flash) {
+
+		Cliente cliente = null;
+
+		if (id > 0) {
+			cliente = clienteService.findOne(id);
+			if (cliente == null) {
+				flash.addFlashAttribute("error", "El ID del cliente no existe en la BBDD!");
+				return "redirect:/listar";
+			}
+		} else {
+			flash.addFlashAttribute("error", "El ID del cliente no puede ser cero!");
+			return "redirect:/listar";
+		}
+		model.put("cliente", cliente);
+		model.put("titulo", "Editar Cliente");
+		return "form";
+	}
+
+	@RequestMapping(value = "/form", method = RequestMethod.POST)
+	public String guardar(@Valid Cliente cliente, BindingResult result, Model model,
+			@RequestParam("file") MultipartFile foto, RedirectAttributes flash, SessionStatus status) {
+
 		if (result.hasErrors()) {
 			model.addAttribute("titulo", "Formulario de Cliente");
 			return "form";
@@ -136,47 +147,26 @@ public class ClienteController {
 
 		String mensajeFlash = (cliente.getId() != null) ? "Cliente editado con éxito!" : "Cliente creado con éxito!";
 
-		clienteService.guardarCliente(cliente);
+		clienteService.save(cliente);
 		status.setComplete();
 		flash.addFlashAttribute("success", mensajeFlash);
 		return "redirect:listar";
 	}
-	
-	@GetMapping("/form/{id}")
-	public String editar(@PathVariable(value = "id") Long id, Map<String, Object> model, RedirectAttributes flash) {
-		Cliente cliente = null;
-		
-		if(id > 0) {
-			cliente = clienteService.findOne(id);
-			if(cliente == null) {
-				flash.addFlashAttribute("error", "El id del cliente no existe en la DB");
-			}
-		}else {
-			flash.addFlashAttribute("error", "ID NO ENCONTRADO");
-			return "redirect:/clientes/listar";
-		}
-		model.put("cliente", cliente);
-		model.put("titulo", "Editar Cliente");
-		return "form";
-	}
-	
-	@GetMapping("/eliminar/{id}")
+
+	@RequestMapping(value = "/eliminar/{id}")
 	public String eliminar(@PathVariable(value = "id") Long id, RedirectAttributes flash) {
-		if(id > 0) {
+
+		if (id > 0) {
 			Cliente cliente = clienteService.findOne(id);
-			clienteService.eliminar(id);
-			flash.addFlashAttribute("success", "Cliente Eliminado con éxito");
-			
-			Path rootPath = Paths.get("uploads").resolve(cliente.getFoto()).toAbsolutePath();
-			File archio = rootPath.toFile();
-			
-			if(archio.exists() && archio.canRead()) {
-				if(archio.delete()) {
-					flash.addFlashAttribute("info", "Foto " + cliente.getFoto() + " eliminada con exito");
-				}
+
+			clienteService.delete(id);
+			flash.addFlashAttribute("success", "Cliente eliminado con éxito!");
+
+			if (uploadFileService.delete(cliente.getFoto())) {
+				flash.addFlashAttribute("info", "Foto " + cliente.getFoto() + " eliminada con exito!");
 			}
+
 		}
-		return "redirect:/clientes/listar";
+		return "redirect:/listar";
 	}
-	
 }
--- 29.1 9_spring-boot-data-jpa[gran_total].zip_expanded/spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/models/entity/Cliente.java
+++ 29.1 9_spring-boot-data-jpa[gran_total].zip_expanded/spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/models/entity/Cliente.java
@@ -1,14 +1,18 @@
-package com.spingboot.app.models.domain;
+package com.bolsadeideas.springboot.app.models.entity;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.PrePersist;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -22,18 +26,16 @@ import org.springframework.format.annotation.DateTimeFormat;
 @Table(name = "clientes")
 public class Cliente implements Serializable {
 
-	private static final long serialVersionUID = -7958281620691135952L;
-
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	@NotEmpty
-	private String nombres;
-	
+	private String nombre;
+
 	@NotEmpty
-	private String apellidos;
-	
+	private String apellido;
+
 	@NotEmpty
 	@Email
 	private String email;
@@ -41,24 +43,18 @@ public class Cliente implements Serializable {
 	@NotNull
 	@Column(name = "create_at")
 	@Temporal(TemporalType.DATE)
-	@DateTimeFormat(pattern = "yyyy-mm-dd")
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private Date createAt;
-	
+
+	@OneToMany(mappedBy = "cliente", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	private List<Factura> facturas;
+
 	private String foto;
 
-	public String getFoto() {
-		return foto;
+	public Cliente() {
+		facturas = new ArrayList<Factura>();
 	}
 
-	public void setFoto(String foto) {
-		this.foto = foto;
-	}
-
-	@PrePersist
-	public void prePersist() {
-		createAt = new Date();
-	}
-	
 	public Long getId() {
 		return id;
 	}
@@ -67,20 +63,20 @@ public class Cliente implements Serializable {
 		this.id = id;
 	}
 
-	public String getNombres() {
-		return nombres;
+	public String getNombre() {
+		return nombre;
 	}
 
-	public void setNombres(String nombres) {
-		this.nombres = nombres;
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
 	}
 
-	public String getApellidos() {
-		return apellidos;
+	public String getApellido() {
+		return apellido;
 	}
 
-	public void setApellidos(String apellidos) {
-		this.apellidos = apellidos;
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
 	}
 
 	public String getEmail() {
@@ -98,21 +94,31 @@ public class Cliente implements Serializable {
 	public void setCreateAt(Date createAt) {
 		this.createAt = createAt;
 	}
-	
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+
+	public List<Factura> getFacturas() {
+		return facturas;
+	}
+
+	public void setFacturas(List<Factura> facturas) {
+		this.facturas = facturas;
+	}
+
+	public void addFactura(Factura factura) {
+		facturas.add(factura);
+	}
+
 	public static long getSerialversionuid() {
 		return serialVersionUID;
 	}
 
-	public Cliente() {
-		// TODO Auto-generated constructor stub
-	}
-
-	public Cliente(Long id, String nombres, String apellidos, String email, Date createAt) {
-		this.id = id;
-		this.nombres = nombres;
-		this.apellidos = apellidos;
-		this.email = email;
-		this.createAt = createAt;
-	}
+	private static final long serialVersionUID = 1L;
 
 }
