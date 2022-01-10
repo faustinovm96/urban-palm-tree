3d8730c7cc5e0ca624fe925ba244830960a48b11
faustinovm96 <fvillasboa96@gmail.com>
2022-01-10 16:20:09 -0300

    CommandLineRunner añadido

--- spring-boot-foto-final/src/main/java/com/spingboot/app/SpringBootDatajpaApplication.java
+++ spring-boot-foto-final/src/main/java/com/spingboot/app/SpringBootDatajpaApplication.java
@@ -1,13 +1,26 @@
 package com.spingboot.app;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class SpringBootDatajpaApplication {
+import com.spingboot.app.models.service.IUploadFileService;
 
+@SpringBootApplication
+public class SpringBootDatajpaApplication implements CommandLineRunner{
+
+	@Autowired
+	private IUploadFileService upload;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDatajpaApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		upload.deleteAll();
+		upload.init();	
 	}
 
 }
